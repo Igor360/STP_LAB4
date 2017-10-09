f6cc698adde805af13ea84748995524552ee8750
Igor360 <igor.dawidencko@gmail.com>
2017-10-09 10:49:35 +0300

    Add test

---
+++ src/test/java/ArrayTest.java
@@ -0,0 +1,22 @@
+import Sorting.Realization.BubbleSort;
+import array.Array;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArrayTest {
+
+    Array arr = null;
+    @Before
+    public void init() {
+        arr = new Array(10);
+    }
+
+    @Test
+    public void SortTest() {
+
+    }
+}
